cb8ea68d05a6e6e6fd0f9cf6401ef405f772a616
ThomasKroonV2 <89969675+ThomasKroonV2@users.noreply.github.com>
2022-03-03 12:00:43 +0100

    Add project files.

---
+++ Opdracht 1/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] zin = new string[6];
+zin[0] = "Peter";
+zin[1] = "is";
+zin[2] = "de";
+zin[3] = "broer";
+zin[4] = "van";
+zin[5] = "hans";
+foreach (var i in zin)
+{
+    Console.Write($"{i} ");
+}
+
+//enters
+Console.WriteLine();
+Console.WriteLine();
+
+//switch var
+string sen = zin[5];
+zin[5] = zin[0];
+zin[0] = sen;
+foreach (var i in zin)
+{
+    Console.Write($"{i} ");
+}
---
+++ Opdracht 2/Program.cs
@@ -0,0 +1,27 @@
+﻿int[,] matrix = new int[3, 3];
+int teller = 1;
+
+//array vullen
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        matrix[i, j] = teller;
+        teller++;
+    }
+}
+
+//array weergeven
+int totaal = 0;
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+
+        totaal = totaal + matrix[i, j]; ;
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine($"Totaal: {totaal}");
